978890654492b707c85681b0f26e7559456c443f
Osatohanmwenaigbogun <osatohanmweaigbogun@gmail.com>
2023-09-10 18:01:05 -0400

    testiing public repo

---
+++ auto.sh
@@ -0,0 +1,4 @@
+export USERNAME=peter
+export PASSWORD=ctfeaugust1122023
+sudo mkdir -p /opt/testing
+sudo echo this is my USERNAME $USERNAME and my PASSWORD $PASSWORD >> credentials.txt
---
+++ welcome.py
@@ -0,0 +1,1 @@
+print("welcomce to CTFE")
